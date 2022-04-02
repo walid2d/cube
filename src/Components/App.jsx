7015d38ae381e74e6dc66b5f703f970ba3aad289
Abdur R. Walid <ar_olid@outlook.com>
2022-04-01 21:04:01 -0600

    first commit

---
+++ src/Components/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  onSearch = (term) => {
+    console.log(term);
+  };
+  render() {
+    return <div>h1</div>;
+  }
+}
+export default App;
